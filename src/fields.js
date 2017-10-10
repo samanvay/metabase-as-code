065f2019692c85d329b928d19de169f940298dd4
Vinay Venu <ijkvinay@gmail.com>
2017-10-10 21:57:06 +0530

    Fix foreign keys in all keys of database

--- src/fields.js
+++ src/fields.js
@@ -10,36 +10,70 @@ const getTableFields = (context, table) => {
 };
 
 const findByName = (items, item) => {
+    if (!item) return null;
     return _.find(items, (it) => it.name === item.name);
 };
 
+const findById = (list, id) => {
+    return _.find(list, (item) => item.id === id);
+};
 
 const syncFields = (sourceContext, targetContext) => {
-    const findTargetField = (sourceField, sourceTable) => {
-        return findByName(findByName(targetContext.tables, sourceTable).fields, sourceField);
+    const findTargetFieldIdBySourceId = (id) => {
+        if (!id) return null;
+        const sourceTable = _.find(sourceContext.tables, (table) => _.find(table.fields, (field) => field.id === id));
+        const sourceField = _.find(sourceTable.fields, (field) => field.id === id);
+        const targetTable = _.find(targetContext.tables, (table) => table.name === sourceTable.name);
+        let targetField = _.find(targetTable.fields, (field) => field.name === sourceField.name);
+        return _.get(targetField, 'id') || null;
     };
 
     _.map(sourceContext.tables, (table) => {
-        join(getTableFields(sourceContext, table), getTableFields(targetContext, findByName(targetContext.tables, table)),
+        return join(getTableFields(sourceContext, table), getTableFields(targetContext, findByName(targetContext.tables, table)),
             (sourceTableFields, targetTableFields) => {
-                const fieldValuesToBeUpdated = _.map(sourceTableFields, (sourceTableField) => {
+                const targetFieldsToBeUpdated = _.map(sourceTableFields, (sourceTableField) => {
                     const targetTableField = findByName(targetTableFields, sourceTableField);
-                    const fieldValueToBeUpdated = {
+                    let fieldValueToBeUpdated = {
                         id: targetTableField.id,
                         description: sourceTableField.description,
                         special_type: sourceTableField.special_type,
                         caveats: sourceTableField.caveats,
-                        fk_target_field_id: findTargetField(sourceTableField, table).id,
+                        fk_target_field_id: findTargetFieldIdBySourceId(sourceTableField.fk_target_field_id),
                         visibility_type: sourceTableField.visibility_type,
                         preview_display: sourceTableField.preview_display,
-                        display_name: sourceTableField.display_name
+                        display_name: sourceTableField.display_name,
                     };
                     return fieldValueToBeUpdated;
                 });
 
-                return Promise.all(_.map(fieldValuesToBeUpdated, (fieldValueToBeUpdated) => {
-                    gateway.put(`${targetContext.url}/api/field/${fieldValueToBeUpdated.id}`, fieldValueToBeUpdated, targetContext.authToken)
+                const updateTargetFields = () => {
+                    return Promise.all(_.map(targetFieldsToBeUpdated, (field) => {
+                        return gateway.put(`${targetContext.url}/api/field/${field.id}`, field, targetContext.authToken)
+                    }));
+                };
+
+                const targetDimensionsToBeUpdated = _.compact(_.map(sourceTableFields, (sourceTableField) => {
+                    const targetTableField = findByName(targetTableFields, sourceTableField);
+                    if (sourceTableField.dimensions === []) return [];
+                    if (_.has(sourceTableField, 'dimensions.human_readable_field_id')) {
+                        const targetFieldId = findTargetFieldIdBySourceId(sourceTableField.dimensions.human_readable_field_id);
+
+                        return {
+                            id: targetTableField.id,
+                            name: sourceTableField.dimensions.name,
+                            type: sourceTableField.dimensions.type,
+                            human_readable_field_id: targetFieldId
+                        };
+                    }
                 }));
+
+                const updateTargetDimensions = () => {
+                    return Promise.all(_.map(targetDimensionsToBeUpdated, (dimension) => {
+                        return gateway.post(`${targetContext.url}/api/field/${dimension.id}/dimension`, dimension, targetContext.authToken)
+                    }))
+                };
+
+                return updateTargetFields().then(updateTargetDimensions);
             }
         );
     })
